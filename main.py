5c7183c83d09adbe992b451346c0386435a9db3c
Sushant Garudkar <garudkar.sa@gmail.com>
2026-04-17 20:51:31 +0530

    minimum edit distance

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
+from utils import Grid
+from utils import DELETION_COST, REPLACE_COST, INSERTION_COST
+
+
+def min_edit_distance(distance_matrix: Grid, source: str, target: str) -> int:
+    n = len(source)
+    m = len(target)
+    
+    distance_matrix[0, 0] = 0
+
+    for i in range(1, n+1):
+        distance_matrix[i, 0] = distance_matrix[i - 1, 0] + DELETION_COST
+    
+    for j in range(1, m+1):
+        distance_matrix[0, j] = distance_matrix[0, j - 1] + INSERTION_COST
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            replace_cost_adjusted_for_same_elements = 0 if source[i - 1] == target[j - 1] else REPLACE_COST
+
+            distance_matrix[i, j] = min(
+                distance_matrix[i - 1, j] + DELETION_COST,
+                distance_matrix[i - 1, j - 1] + replace_cost_adjusted_for_same_elements,
+                distance_matrix[i, j - 1] + INSERTION_COST
+            )
+
+    for row in distance_matrix:
+        print(row)
+    
+    return distance_matrix[n, m]
+
+
 def main():
-    print("Hello from coding!")
+    source = input("Enter the source string: ")
+    target = input("Enter the target string: ")
+
+    distance_matrix = Grid(n=len(source) + 1, m=len(target) + 1)
+    min_distance = min_edit_distance(distance_matrix=distance_matrix, source=source, target=target)
+
+    print(f"Minimum edit distance of {source} to {target} is {min_distance}")
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,35 @@
+from typing import List
+
+DELETION_COST = 1
+INSERTION_COST = 1
+REPLACE_COST = 1
+
+def create_2d_list(n: int, m: int) -> List[List[int]]:
+    lst: List[List[int]] = []
+
+    for _ in range(n):
+        temp = []
+
+        for _ in range(m):
+            temp.append(0)
+        
+        lst.append(temp)
+    
+    return lst
+
+class Grid:
+    def __init__(self, n, m) -> None:
+        self.lst = create_2d_list(n, m)
+
+    def __getitem__(self, key):
+        row, col = key
+
+        return self.lst[row][col]
+    
+    def __setitem__(self, key, value: int) -> None:
+        row, col = key
+
+        self.lst[row][col] = value
+    
+    def __iter__(self):
+        return iter(self.lst)
